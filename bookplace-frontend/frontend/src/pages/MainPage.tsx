c96eb70e44f174e1965df95400aead1c69a33830
Hubert Chruścicki <chruscicki.hubert@wp.pl>
2025-05-04 23:23:56 +0200

    feat(api): connected offers load endpoint to frontend page

--- bookplace-frontend/frontend/src/pages/MainPage.tsx
+++ bookplace-frontend/frontend/src/pages/MainPage.tsx
@@ -2,12 +2,32 @@ import { Box } from "@mui/material";
 import Header from "../components/Header/Header";
 import OffersGrid from "../components/Offer/OffersGrid";
 import Footer from "../components/Footer/Footer";
-import { OfferCardModel } from "../models/OfferModel";
-import {useState} from "react";
+import {OfferCardModel, OfferModel} from "../models/OfferModel";
+import {useEffect, useState} from "react";
+import api from "../api/axiosApi.ts";
+import {useLocation} from "react-router-dom";
 
 const MainPage: React.FC = () => {
 
     const [offers, setOffers] = useState<OfferCardModel[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        api.get("/offers/load-offers/?limit=48&offset=0")
+            .then(res => {
+                setOffers(res.data.results);
+                setError(null);
+            })
+            .catch(err => {
+                console.error("Error during loading offers data:", err)
+                setError("Cannot load offers");
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, []);
 
     return (
         <Box
